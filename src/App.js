e114d2d81ee4521a248279950021d6a1e6e7dee0
unknown <rajkumar.d408@gmail.com>
2022-05-18 15:46:57 +0530

    push

--- src/App.js
+++ src/App.js
@@ -3,21 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+     <div className='quiz-container'>
+       <p>Question</p>
+       <div className="quiz-option">
+
+       </div>
+Hello
+     </div>
     </div>
   );
 }
