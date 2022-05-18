a2e01556445ff7ba890a358b54c07981615ca9f5
raj kumar <rajkumar.d408@gmail.com>
2022-05-18 23:24:40 +0530

    addgame

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import questions from './questions';
+import {useState} from 'react'
 
 function App() {
+  const [questionidx, setQuestionidx] = useState(0);
+  const[score,setScore] =useState(0);
+  const [showScore,setShowScore]=useState(false);
+  const currQuestion = questions[questionidx]
+  const selectoption =(idx) =>{
+console.log(idx)
+if(currQuestion.answer === idx){
+  setScore(score+1)
+  
+}
+  const nextQ = questionidx+1;
+  if(nextQ < questions.length){
+    setQuestionidx(questionidx+1);
+  }
+  else{
+   setShowScore(true)
+  }
+
+
+  }
+  const reset =() =>{
+    setQuestionidx(0);
+    setScore(0);
+setShowScore(false)
+  }
   return (
     <div className="container">
-     <div className='quiz-container'>
-       <p>Question</p>
-       <div className="quiz-option">
-
-       </div>
-Hello
-     </div>
+      {
+        showScore ? (
+        <>
+        <h1>Your score is:
+         {score}</h1><button onClick={reset}>Restart Quiz</button> 
+         </>): (
+          <div className='quiz-container'>
+          <p>{currQuestion.question}</p>
+          <div className="quiz-option">
+          <ul className="quiz-container-ul">
+            {currQuestion.options.map((option,i) =>{
+              return <li className="quiz-container-li" onClick={()=> selectoption(i)}>{option}</li>
+           })}
+          
+          </ul>
+          </div>
+   
+        </div>
+        )
+      }
+    
     </div>
   );
 }
---
+++ src/questions.js
@@ -0,0 +1,28 @@
+export default [
+    {
+        question: "1st question",
+        options:["opt1","opt2","opt3","opt4"],
+        answer:1,
+    },
+    {
+        question: "2st question",
+        options:["opt1","opt2","opt3","opt4"],
+        answer:2,
+    },
+    {
+        question: "3st question",
+        options:["opt1","opt2","opt3","opt4"],
+        answer:3,
+    },
+    {
+        question: "4st question",
+        options:["opt1","opt2","opt3","opt4"],
+        answer:1,
+    },
+    {
+        question: "5st question",
+        options:["opt1","opt2","opt3","opt4"],
+        answer:0,
+    },
+
+]
